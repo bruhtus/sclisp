e03c883f7cd18d40facb7f2fe48a3ee46558efd5
Robertus Chris <hibruhtus@gmail.com>
2025-09-20 12:26:37 +0700

    feat: replace strcmp() in string.h with stringcmp()
    Trying to find out how `strcmp()` works and see if we can use something
    simpler instead. Let's see how it goes.

--- sclisp.c
+++ sclisp.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include <editline/readline.h>
 
 /* #define max_input 2048 */
 
 /* static char input[max_input]; */
+
+int stringcmp(const char *str1, const char *str2);
 
 int main()
 {
@@ -24,7 +25,7 @@ int main()
 
 		char *input = readline(prompt);
 
-		if (input == NULL || strcmp(input, "exit") == 0) {
+		if (input == NULL || stringcmp(input, "exit") == 0) {
 			free(input);
 			break;
 		}
@@ -34,4 +35,25 @@ int main()
 	}
 
 	return 0;
+}
+
+/*
+ * References:
+ * - https://stackoverflow.com/a/67520047
+ * - https://www.reddit.com/r/learnprogramming/comments/7xzaiv/comment/ducbt4y
+ *
+ * Strings literals are pointers in C.
+ * When you see a sentence like "Hello world!",
+ * that variable is of the type const char*,
+ * meaning it is a pointer to a bunch of chars that cannot be modified.
+ */
+int stringcmp(const char *str1, const char *str2)
+{
+	while (*str1 && *str1 == *str2) {
+		str1++;
+		str2++;
+	}
+
+	int result = (unsigned char)*str1 - (unsigned char)*str2;
+	return result;
 }
