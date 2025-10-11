f5402cf4234bc1fa6223a69737940e692f4ce622
Robertus Chris <hibruhtus@gmail.com>
2025-10-12 04:43:41 +0700

    chore(playground): add example of segfault when address expired
    This is another case of segfault when the virtual memory address already
    freed or not exist after the function return. One way to make sure
    the virtual memory address still exist after the function return, we
    can turn the variable into static variable, which still persist after
    the function return.

--- playground/segfault-playground.c
+++ playground/segfault-playground.c
@@ -1,9 +1,21 @@
+int *something()
+{
+	/* static int a = 69; */
+	int a = 69; // The virtual memory of this variable get freed after the function return.
+	return &a;
+}
+
 int main()
 {
 	/* char *str = "ANU"; */
 	/* str[0] = 'B'; */
 
-	int *a, b;
-	/* a = &b; */
-	*a = 69;
+	/* int *a, b; */
+	/* /1* a = &b; *1/ */
+	/* *a = 69; */
+
+	int *a = something();
+	*a = 42;
+
+	return 0;
 }
