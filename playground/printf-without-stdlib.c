fcf22671ff527e8d1bb901bfd2f858c5233dfb96
Robertus Chris <hibruhtus@gmail.com>
2025-10-28 05:24:33 +0700

    chore(playground): use embedded assembly to replace write()
    This is the first attempt to not use any library. So instead of using
    standard library, we use embedded assembly. At the moment, still have no
    idea how assembly syntax is, so i need to learn that later on.
    
    The problem with this approach is that the binary size is too big, it's
    because the compiler still including the standard library in the binary
    when we are using `-static` flag (?), at least that's according to the
    youtube tutorial. So we will tackle that after this.
    
    There's a subtle difference between this code and the tutorial code.
    Instead of using `const char holy[]`, i am using `const char *holy` and
    that lead to the change using `mov %0, %%rsi` instead of `lea %0,
    %%rsi`. Still not sure what the difference between `mov` and `lea`, but
    it seems like `mov` is the instruction to move data between registers
    and memory? We will need to find that out later on.

--- playground/printf-without-stdlib.c
+++ playground/printf-without-stdlib.c
@@ -6,12 +6,25 @@
  * like this:
  * gcc -static -o playground/printf-without-stdlib playground/printf-without-stdlib.c
  */
-#include <unistd.h>
-
 int main(void)
 {
 	const char *holy = "Holy C!\n";
 
-	write(STDOUT_FILENO, holy, sizeof(holy));
+	/*
+	 * Reference:
+	 * - %0 means: https://stackoverflow.com/a/10461830
+	 * - embedded assembler: http://wiki.osdev.org/Inline_Assembly
+	 */
+	asm volatile (
+		"mov $1, %%rax\n\t" // syscall number to write.
+		"mov $1, %%rdi\n\t" // stdout (fd = 1).
+		"mov %0, %%rsi\n\t" // use the first variable content.
+		"mov $8, %%rdx\n\t" // characters length.
+		"syscall\n\t"
+		:
+		: "m"(holy)
+		: "%rax", "%rdi", "%rsi", "%rdx"
+	);
+
 	return 0;
 }
