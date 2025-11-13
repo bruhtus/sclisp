15757ef2a2189bbd8d5d299f105ad0f3b793417d
Robertus Chris <hibruhtus@gmail.com>
2025-11-13 14:56:41 +0700

    refactor: move enum, struct, and eval function to utils.{h,c}
    Recently learned about C library development, and i want to try out
    using header and C file like the C library code.
    
    With that in mind, move out all enum and struct to utils.h and all
    function prototype and function definition (except main function) to
    utils.c.
    
    Also, currently we don't use static library for utils.c, so we adjust
    the run script to also compile utils.c.
    
    I noticed that compiling using this method is a little bit slower
    compared to compiling in one big files. Most likely because we are
    including `mpc.h` in `utils.h` and we are including `utils.h` in both
    `utils.c` and `sclisp.c` so twice the effort for the compiler (?). Still
    not sure, need more info.
    
    Let's see how it goes.

---
+++ utils.h
@@ -0,0 +1,52 @@
+#include "mpc.h"
+
+enum lval_type {
+	LVAL_MALLOC_FAILED = -2,
+	LVAL_ERR = -1,
+	LVAL_NUM = 1,
+	LVAL_SYM = 2,
+	LVAL_SEXPR = 3
+};
+
+enum lval_err {
+	LERR_INVALID_SYM = 0,
+	LERR_INVALID_NUM = 1,
+	LERR_DIV_ZERO = 2,
+	LERR_MODULO_NAN = 3,
+	LERR_POW_NAN = 4
+};
+
+/* Lisp value. */
+struct lval {
+	enum lval_type type;
+	double num;
+	const char *err;
+	char *sym;
+	int count;
+	struct lval **cell;
+};
+
+struct lval *lval_eval(struct lval *value);
+struct lval *lval_eval_sexpr(struct lval *value);
+struct lval *builtin_op(struct lval *value, char *op);
+
+struct lval *lval_take(struct lval *value, int i);
+struct lval *lval_pop(struct lval *value, int i);
+struct lval *lval_read(mpc_ast_t *ast);
+
+void lval_println(struct lval *value);
+void lval_expr_print(struct lval *v, char open, char close);
+void lval_print(struct lval *value);
+
+struct lval *lval_add(struct lval *v1, struct lval *v2);
+void lval_del(struct lval *value);
+
+struct lval *lval_err(const char *mes);
+struct lval *lval_read_num(mpc_ast_t *ast);
+struct lval *lval_num(double num);
+struct lval *lval_sym(char *sym);
+struct lval *lval_sexpr(void);
+
+struct lval *malloc_err(void);
+
+int stringcmp(const char *str1, const char *str2);
