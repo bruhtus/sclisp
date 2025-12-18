bedd47be9b159dadf2cccbb536e139c45cec1e50
Robertus Chris <hibruhtus@gmail.com>
2025-12-18 09:08:01 +0700

    chore(playground): reproduce the bad example of variadic function
    Previously i can not reproduce the bad example of using variadic
    function when i use 1 variadic argument. Now when i tried using 2
    variadic arguments, i can reproduce the gibberish that was printed out
    on the second vprintf().
    
    Still not sure why we need at least 2 variadic arguments to trigger the
    "undefined behavior".

--- playground/variadic-function.c
+++ playground/variadic-function.c
@@ -2,10 +2,20 @@
 #include <stdarg.h>
 
 void variadic_print(const char *, ...);
+void variadic_process(const char *, va_list);
 
+/*
+ * Reference:
+ * https://stackoverflow.com/a/37789384
+ */
 int main(void)
 {
-	variadic_print("anu %s\n", "itu");
+	/*
+	 * This is an example to trigger the bad scenario.
+	 * Still not sure why using 2 variadic arguments
+	 * work, but not using 1 variadic argument.
+	 */
+	variadic_print("%s %s\n", "anu", "itu");
 
 	return 0;
 }
@@ -15,8 +25,32 @@ void variadic_print(const char *fmt, ...)
 	va_list ap;
 	va_start(ap, fmt);
 
-	vprintf(fmt, ap);
-	vprintf(fmt, ap);
+	variadic_process(fmt, ap);
 
 	va_end(ap);
+}
+
+/*
+ * To run the bad example, we can use this command:
+ * gcc -DBAD -o playground/variadic-function playground/variadic-function.c
+ *
+ * The BAD is an example of using variadic arguments
+ * multiple times, which can result in
+ * __undefined behavior__.
+ *
+ * To prevent that, we can copy the variadic arguments and
+ * use the copy value instead.
+ */
+void variadic_process(const char *fmt, va_list ap)
+{
+#ifdef BAD
+	vprintf(fmt, ap);
+#else
+	va_list apc;
+	va_copy(apc, ap);
+	vprintf(fmt, apc);
+	va_end(apc);
+#endif
+
+	vprintf(fmt, ap);
 }
