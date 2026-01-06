5bdaf3dca53005580e85eaad13f667029b64f891
Robertus Chris <hibruhtus@gmail.com>
2026-01-07 03:33:47 +0700

    chore(playground): add example using sizeof with __FILE__ macro
    Seems like __FILE__ macro expands in the form of C string constant,
    which is why we can use `sizeof` operator with it.

--- playground/length-file-line-macro.c
+++ playground/length-file-line-macro.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+/*
+ * Reference:
+ * https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Predefined-Macros.html
+ */
 int main(void)
 {
 	printf("__FILE__: %s\n", __FILE__);
-	printf("__FILE__: %d\n", strlen(__FILE__));
+	printf("__FILE__ strlen: %d\n", strlen(__FILE__));
+	printf("__FILE__ sizeof: %d\n", sizeof(__FILE__));
 
 	/*
 	 * Reference:
@@ -13,7 +18,7 @@ int main(void)
 	int line_len = snprintf(NULL, 0, "%d", __LINE__);
 
 	printf("__LINE__: %d\n", __LINE__);
-	printf("__LINE__: %d\n", line_len);
+	printf("__LINE__ length: %d\n", line_len);
 
 	return 0;
 }
